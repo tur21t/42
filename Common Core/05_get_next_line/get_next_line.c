5a111eaad758db6d2ff1bc4c36ea06f9e86a6532
Dmitrii Tur <dmtur@cbr10s5.42barcelona.com>
2025-08-19 23:13:15 +0200

    get next line is added

--- Common Core/05_get_next_line/get_next_line.c
+++ Common Core/05_get_next_line/get_next_line.c
@@ -6,38 +6,129 @@
 /*   By: dmtur <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/11 15:42:58 by dmtur             #+#    #+#             */
-/*   Updated: 2025/08/12 19:13:56 by dmtur            ###   ########.fr       */
+/*   Updated: 2025/08/19 22:33:56 by dmtur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include "get_next_line.h" 
 
-void	ft_putstr(char *str)
+static int	find_newline(char *s)
 {
 	int	i;
 
 	i = 0;
-	while (str[i])
+	if (!s)
+		return (-1);
+	while (s[i])
+	{
+		if (s[i] == '\n')
+			return (i);
 		i++;
-	write(1, str, i);
+	}
+	return (-1);
 }
 
-#include <fcntl.h>
-
-int	main(void)
+static char	*extract_line(char **storage, int nl_index)
 {
-	int	fd;
-	char		buffer[5];
-	ssize_t	bytes_read;
+	char	*line;
+	char	*rest;
+	int		len;
 
-	fd = open("text.txt", O_RDONLY);
-	if (fd < 0)
-		return (1);
-	bytes_read = read(fd, buffer, 5);
-	if (bytes_read > 0)
-       		write(1, buffer, bytes_read);
-	close(fd);
+	if (nl_index >= 0)
+	{
+		line = ft_strndup(*storage, nl_index + 1);
+		len = ft_strlen(*storage + nl_index + 1);
+		rest = ft_strndup(*storage + nl_index + 1, len);
+		free(*storage);
+		*storage = rest;
+	}
+	else
+	{
+		line = ft_strndup(*storage, ft_strlen(*storage));
+		free(*storage);
+		*storage = NULL;
+	}
+	return (line);
+}
 
-	ft_putstr("\n\n SARVA MANGALAM\n\n\n");
+static int	append_buffer(char **storage, char *buffer)
+{
+	char	*tmp;
+
+	if (!*storage)
+		*storage = ft_strndup(buffer, ft_strlen(buffer));
+	else
+	{
+		tmp = ft_strjoin(*storage, buffer);
+		free(*storage);
+		*storage = tmp;
+	}
 	return (0);
 }
+
+static int	read_into_storage(int fd, char **storage)
+{
+	ssize_t	bytes;
+	char	*buffer;
+
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (-1);
+	bytes = 1;
+	while (bytes > 0 && find_newline(*storage) == -1)
+	{
+		bytes = read(fd, buffer, BUFFER_SIZE);
+		if (bytes > 0)
+		{
+			buffer[bytes] = '\0';
+			append_buffer(storage, buffer);
+		}
+	}
+	free(buffer);
+	if (bytes < 0)
+		return (-1);
+	return (0);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*storage;
+	int			nl_index;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	if (read_into_storage(fd, &storage) == -1)
+		return (NULL);
+	if (!storage || *storage == '\0')
+	{
+		free(storage);
+		storage = NULL;
+		return (NULL);
+	}
+	nl_index = find_newline(storage);
+	return (extract_line(&storage, nl_index));
+}
+/*
+#include <fcntl.h>
+#include <stdio.h>
+
+int main(void)
+{
+	int fd;
+	char *line;
+
+	fd = open("text.txt", O_RDONLY);
+	line = get_next_line(fd);
+//  -one line check-
+//	printf("line: %s", line);
+//	free(line);
+	while ((line = get_next_line(fd)) != NULL)
+	{
+
+		printf("line: %s", line);
+		free(line);
+	}
+
+	close(fd);
+	return (0);
+}*/
---
+++ Common Core/05_get_next_line/get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmtur <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/15 18:42:26 by dmtur             #+#    #+#             */
+/*   Updated: 2025/08/19 18:18:18 by dmtur            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 121
+# endif
+
+int		ft_strlen(char *s);
+char	*ft_strndup(char *s, int n);
+char	*ft_strjoin(char *s1, char *s2);
+char	*get_next_line(int fd);
+
+#endif
