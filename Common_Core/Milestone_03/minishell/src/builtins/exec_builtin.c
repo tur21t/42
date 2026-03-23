60b6ff163bff3f32734585fe8205fa9bee0d06d6
Dmitrii Tur <dmtur@car10s6.42barcelona.com>
2026-03-23 16:04:00 +0100

    Echo functionality is added. strcmp is replaced with ft_strncmp

--- Common_Core/Milestone_03/minishell/src/builtins/exec_builtin.c
+++ Common_Core/Milestone_03/minishell/src/builtins/exec_builtin.c
@@ -17,9 +17,11 @@ int	exec_builtin(t_shell *shell, t_cmd *cmd)
 	(void)shell;
 	if (!cmd || !cmd->args || !cmd->args[0])
 		return (1);
-	if (strcmp(cmd->args[0], "exit") == 0)
+	if (ft_strncmp(cmd->args[0], "echo", 5) == 0)
+    	return builtin_echo(cmd->args);
+	if (ft_strncmp(cmd->args[0], "exit", 5) == 0)
 		return (builtin_exit());
-	if (strcmp(cmd->args[0], "pwd") == 0)
+	if (ft_strncmp(cmd->args[0], "pwd", 4) == 0)
 		return (builtin_pwd());
 	return (1);
 }
@@ -28,9 +30,11 @@ int	is_builtin(char *cmd)
 {
 	if (!cmd)
 		return (0);
-	if (strcmp(cmd, "exit") == 0)
+	if (ft_strncmp(cmd, "echo", 5) == 0)
+    	return (1);	
+	if (ft_strncmp(cmd, "exit", 5) == 0)
 		return (1);
-	if (strcmp(cmd, "pwd") == 0)
+	if (ft_strncmp(cmd, "pwd", 4) == 0)
 		return (1);
 	return (0);
 }
