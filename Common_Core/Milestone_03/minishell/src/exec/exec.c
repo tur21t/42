18e7b03bf68fd370ed79c53661945f18a61c1d3d
Dmitrii Tur <dmtur@car8s6.42barcelona.com>
2026-04-20 12:54:37 +0200

    now redirs work on exec.c function

--- Common_Core/Milestone_03/minishell/src/exec/exec.c
+++ Common_Core/Milestone_03/minishell/src/exec/exec.c
@@ -12,19 +12,43 @@
 
 #include "minishell.h"
 
+static void	exec_single_builtin(t_shell *shell, t_cmd *cmd)
+{
+	int	in;
+	int	out;
+
+	in = dup(STDIN_FILENO);
+	out = dup(STDOUT_FILENO);
+	if (in < 0 || out < 0)
+		return ;
+	if (apply_redirections(cmd, shell) == -1)
+	{
+		dup2(in, STDIN_FILENO);
+		dup2(out, STDOUT_FILENO);
+		close(in);
+		close(out);
+		return ;
+	}
+	exec_builtin(shell, cmd);
+	dup2(in, STDIN_FILENO);
+	dup2(out, STDOUT_FILENO);
+	close(in);
+	close(out);
+}
+
 void	execute(t_shell *shell, t_cmd *cmds)
 {
-    int	n_cmds;
+	int	n_cmds;
 
-    if (!cmds)
-        return ;
-    n_cmds = init_pipeline(cmds);
-    if (n_cmds == 1 && cmds->args && cmds->args[0] && is_builtin(cmds->args[0]))
-    {
-        exec_builtin(shell, cmds);
-        return ;
-    }
-    execute_pipeline(shell, cmds, n_cmds);
+	if (!cmds)
+		return ;
+	n_cmds = init_pipeline(cmds);
+	if (n_cmds == 1 && cmds->args && cmds->args[0] && is_builtin(cmds->args[0]))
+	{
+		exec_single_builtin(shell, cmds);
+		return ;
+	}
+	execute_pipeline(shell, cmds, n_cmds);
 }
 
 /*void	execute(t_shell *shell, t_cmd *cmds)
