205a8df53a861a0f1473ab25f928cc30e95a8f45
Dmitrii Tur <dmtur@car8s3.42barcelona.com>
2026-05-13 15:17:58 +0200

    parser folder norminette fix_02

--- Common_Core/Milestone_03/minishell/src/parser/parser_support02.c
+++ Common_Core/Milestone_03/minishell/src/parser/parser_support02.c
@@ -35,43 +35,6 @@ void	print_cmds(t_cmd *cmds)
 	}
 }
 
-// void	free_redirs(t_redir *redir)
-// {
-// 	t_redir	*tmp;
-
-// 	while (redir)
-// 	{
-// 		tmp = redir->next;
-// 		free(redir->file);
-// 		free(redir);
-// 		redir = tmp;
-// 	}
-// }
-
-// void	free_cmds(t_cmd *cmds)
-// {
-// 	t_cmd	*tmp;
-// 	int		i;
-
-// 	while (cmds)
-// 	{
-// 		tmp = cmds->next;
-// 		if (cmds->args)
-// 		{
-// 			i = 0;
-// 			while (cmds->args[i])
-// 			{
-// 				free(cmds->args[i]);
-// 				i++;
-// 			}
-// 			free(cmds->args);
-// 		}
-// 		free_redirs(cmds->redirs);
-// 		free(cmds);
-// 		cmds = tmp;
-// 	}
-// }
-
 int	check_syntax(t_token *tokens)
 {
 	t_token	*prev;
